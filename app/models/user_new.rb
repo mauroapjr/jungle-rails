b997df4b8bc76af4dc4458bd11f540e0f4e93c41
mauroapjr <mauroapjr@outlook.com>
2023-03-10 22:42:05 -0500

    add login/signup feature

---
+++ app/models/user_new.rb
@@ -0,0 +1,2 @@
+class UserNew < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get 'admin/categories'
   get 'about/index'
+  
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
