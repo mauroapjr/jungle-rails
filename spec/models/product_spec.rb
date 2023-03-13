0cce443e0be7daf5ea879d1a17c980693c5d187e
mauroapjr <mauroapjr@outlook.com>
2023-03-13 17:40:30 -0400

    fix the first test and add test for add/products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   
-    describe 'Validations' do
+  describe 'Validations' do
 
-      it "check if the name exist" do
-        @category = Category.create(name: "Shrubs")
-        @product = Product.create(name: 'lilys', price: 20, quantity: 4, category: @category)
-        expect(@product).to be_persisted
+    it "check if the name exist" do
+      @category = Category.create(name: "Shrubs")
+      @product = Product.create(quantity:12, price: 12, category: @category)
+      expect(@product.name).to be_nil
+      expect(@product.errors.messages[:name]).to include('can\'t be blank')
+    end
+
+    it "check if creates a new products with all fields" do
+      @category = Category.create(name: "Shrubs")
+      @product = Product.create(name: 'lilys', price: 20, quantity: 4, category: @category)
+      expect(@product).to be_persisted
     end
   end
 
