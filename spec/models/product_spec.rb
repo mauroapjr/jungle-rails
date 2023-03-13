19a18fb9cca25d4c533dd10faa8d956021bd0a38
mauroapjr <mauroapjr@outlook.com>
2023-03-13 17:53:26 -0400

    add test for products/category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,17 +4,11 @@ RSpec.describe Product, type: :model do
   
   describe 'Validations' do
 
-    it "check if the name exist" do
+    it "Check if the name exist" do
       @category = Category.create(name: "Shrubs")
       @product = Product.create(quantity:12, price: 12, category: @category)
       expect(@product.name).to be_nil
       expect(@product.errors.messages[:name]).to include('can\'t be blank')
-    end
-
-    it "check if creates a new products with all fields" do
-      @category = Category.create(name: "Shrubs")
-      @product = Product.create(name: 'lilys', price: 20, quantity: 4, category: @category)
-      expect(@product).to be_persisted
     end
 
     it "Check if there is a price" do
@@ -24,6 +18,25 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.messages[:price]).to include('can\'t be blank')
     end
 
+    it "Check if there is quantity" do 
+      @category = Category.create(name: "Shrubs")
+      @product = Product.create(name: 'lilys', price: 20, category: @category)
+      expect(@product.quantity).to be_nil
+      expect(@product.errors.messages[:quantity]).to include('can\'t be blank')
+    end
+
+    it "Check if there is a category" do
+      @product = Product.create(name: 'lilys', price: 20, quantity: 4)
+      expect(@product.category).to be_nil
+      expect(@product.errors.messages[:category]).to include('can\'t be blank')
+    end
+
+    it "Check if creates a new products with all fields" do
+      @category = Category.create(name: "Shrubs")
+      @product = Product.create(name: 'lilys', price: 20, quantity: 4, category: @category)
+      expect(@product).to be_persisted
+    end
+    
   end
 
 end
