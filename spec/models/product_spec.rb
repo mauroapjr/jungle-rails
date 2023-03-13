2c044430ad4ff948639741d3b138277cb68884bb
mauroapjr <mauroapjr@outlook.com>
2023-03-13 17:46:41 -0400

    add test for products/price

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,6 +16,14 @@ RSpec.describe Product, type: :model do
       @product = Product.create(name: 'lilys', price: 20, quantity: 4, category: @category)
       expect(@product).to be_persisted
     end
+
+    it "Check if there is a price" do
+      @category = Category.create(name: "Shrubs")
+      @product = Product.create(name: 'lilys', quantity: 4, category: @category)
+      expect(@product.price).to be_nil
+      expect(@product.errors.messages[:price]).to include('can\'t be blank')
+    end
+
   end
 
 end
