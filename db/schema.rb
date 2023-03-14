c9374bd434099ac46377fd7c8ea21be8bca9c992
mauroapjr <mauroapjr@outlook.com>
2023-03-13 23:06:06 -0400

    add user/last_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_10_223552) do
+ActiveRecord::Schema.define(version: 2023_03_14_001741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2023_03_10_223552) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
   end
 
   add_foreign_key "line_items", "orders"
